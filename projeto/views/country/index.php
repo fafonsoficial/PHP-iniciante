fcf0afa005373eecb047057243c789fb0a8df53e
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-04-29 19:47:06 +0100

    Organização da minha primeira aula de php

---
+++ projeto/views/country/index.php
@@ -0,0 +1,15 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+?>
+<h1>Countries</h1>
+<ul>
+<?php foreach ($countries as $country): ?>
+    <li>
+        <?= Html::encode("{$country->code} ({$country->name})") ?>:
+        <?= $country->population ?>
+    </li>
+<?php endforeach; ?>
+</ul>
+
+<?= LinkPager::widget(['pagination' => $pagination]) ?>
